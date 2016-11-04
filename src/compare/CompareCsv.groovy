60228bb1db0dadfb090400dc586cedeef810272b
Giuseppe Di Pierri <giuseppe.dipierri@gmtech.ch>
2016-11-04 14:38:58 +0100

    comparing of 2 CSVs or directories (with CSV files) is improved.

--- src/compare/CompareCsv.groovy
+++ src/compare/CompareCsv.groovy
@@ -1,6 +1,9 @@
 package compare
-@Grab('com.xlson.groovycsv:groovycsv:0.2')
+//@Grab('com.xlson.groovycsv:groovycsv:0.2')
+@Grab('com.xlson.groovycsv:groovycsv:1.1')
 import com.xlson.groovycsv.CsvParser
+import static com.xlson.groovycsv.CsvParser.parseCsv
+//import static com.xlson.groovycsv.CsvParser.parseCsv
 
 
 import java.nio.file.Paths
@@ -9,40 +12,110 @@ import java.nio.file.Paths
 class CompareCsv {
 
 	static void main(String[] args){
-		def newData = new CsvParser().parse(new File('csv/gm_scorecards_2015_2016.csv').text)
-		println "read new data: "+newData.size()
-		def oldData = new CsvParser().parse(new File('csv/2015_2016.csv').text)
-		println "read old data: "+oldData.size()
 		
-		def oldLine = oldData.sort{it.ORDERID}.toList().groupBy{it.ORDERID}
-		def list 	= newData.sort{it.ORDERID}.toList()//.subList(0,5)
-		for(line in list) {
-			def oldRecords = oldLine[line.ORDERID]
-			if(oldRecords.size() != 1){
-				println "-------------------------"
-				println "for $line"
-				println "....found: "+oldRecords
-			}else{
-				def old = oldRecords[0]
-				def fields = ["CUSTOMERID","ACCOUNTID","ORDERID","GENDER","LANGUAGE","FIRSTNAME","LASTNAME","STREET","ZIP","CITY","BIRTHDATE","CREDITCHECK","CUSTOMERSEARCH","REQUESTAMOUNT","MERCHANTKEY","MERCHANTLIMIT","AVAILABLECREDIT","CHECKDATE","ORDERDATE","MERCHANTID","MERCHANTNAME","ORDERVALUE","AFIRSTORDER","ALASTORDER","ANUMBERORDERS","AORDERSAMOUNT","AOPENDEBIT","AMAXWARNINGLEVEL","ALASTWARNING","CFIRSTORDER","CLASTORDER","CNUMBERORDERS","CORDERSAMOUNT","COPENDEBIT","CMAXWARNINGLEVEL","CLASTWARNING","EXPORTDATE"]
-				fields.each{
-//					println "--------------------------------"
-//					println "**$it"
-					def orderId = line.ORDERID
-					def oldValue = old[it]
-					def newValue = line[it]
-//					println "$oldValue == $newValue?"
-//					println (oldValue!=newValue)
-					if(oldValue!=newValue)
-						println "$orderId -> $it doesn't match: "+oldValue +" <> "+newValue
-				}
+//		checkFiles(new File('/home/dev/pino/reports/testing/02.11.2015.csv'), new File('/home/dev/pino/reports/new/02.11.2015.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/new/02.11.2015.csv'), new File('/home/dev/pino/reports/testing/02.11.2015.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/testing/12.01.2016.csv'), new File('/home/dev/pino/reports/new/12.01.2016.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/new/12.01.2016.csv'), new File('/home/dev/pino/reports/testing/12.01.2016.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/new/03.05.2016.csv'), new File('/home/dev/pino/reports/testing/03.05.2016.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/new/16.08.2016.csv'), new File('/home/dev/pino/reports/testing/16.08.2016.csv'))
+		checkDirs(new File("/home/dev/pino/reports/testing"), "/home/dev/pino/reports/new")
+	}
+
+	private static String checkDirs(File rootFiles, String newDir) {
+		rootFiles.eachFile(){ File oldFile ->
+			if(oldFile.size() < 36551553 && oldFile.path.contains("csv")){
+				def newFile = new File(newDir+"/"+oldFile.absolutePath.split("/").last())
+				checkFiles(newFile, oldFile)
+				checkFiles(oldFile,	newFile)
 			}
-////			println oldRecords.size()
-//			if(oldRecords.size() == 0)
-//				println "not found: "+line
-////			println matching(oldRecords, line)
-////			println "$line.CUSTOMERID $line.ACCOUNTID"
+		}
+	}
+	
+	static void checkFiles(oldFile, newFile){
+		println "************************* checking '"+oldFile.path
+		println "************************* -against '"+newFile.path+"'"
+		def oldText = oldFile.text
+		def newText = newFile.text
+		def oldList = parseCsv(fix(oldText)).sort{it.ORDERID}.toList()
+		def newList = parseCsv(fix(newText)).sort{it.ORDERID}.toList()
+		
+		def oldOrderIdLine 	= oldList.groupBy{it.ORDERID}
+		def oldPersonLine 	= oldList.groupBy{secondaryKey(it)}
+		def newDateLine 	= newList.groupBy{it.CHECKDATE?.substring(0, 10)}
+		if(newDateLine.keySet().size()>1)
+			println "WARNING: file "+oldFile.path+" contains more dates: "+newDateLine.keySet()
+		for(line in newList) {
+			try {
+				if(line.MERCHANTKEY == 'mobilewalletPaycard')
+					continue;
+				def oldRecords = oldOrderIdLine[line.ORDERID]
+				if(!oldRecords){
+					if(newDateLine[line.CHECKDATE.substring(0, 10)]?.size() != 1){
+						println "-------------------------"
+						println "ERROR: This line is available in file: "+newFile.path +" and not in "+oldFile.path
+						println "==> '$line'"
+					}//else ignore, because in the old line there is a wrong date that we know as wrong already.
+				}else if(oldRecords.size() != 1){
+					def oldPersonRecords = oldPersonLine[secondaryKey(line)]
+					if(oldPersonRecords.size() != 1){
+						println "-------------------------"
+						println "ERROR: This line is available in file: "+newFile.path
+						println "==> '$line'"
+						println "....and $oldRecords.size() times in file '$oldFile.path': "+oldRecords
+					}else{
+						match2Lines(line, oldPersonRecords.first())
+					}
+				}else{
+					match2Lines(line, oldRecords.first())
+				}
+			} catch (Exception e) {
+				e.printStackTrace()
+			}
 		}
 		println "done!"
+
+	}
+
+	private static String secondaryKey(it) {
+		""+it.FIRSTNAME+it.LASTNAME+it.BIRTHDATE+it.CHECKDATE
+	}
+
+	private static match2Lines(line, old) {
+		def fields = ["CUSTOMERID","ACCOUNTID","ORDERID","GENDER","LANGUAGE","FIRSTNAME","LASTNAME","STREET","ZIP","CITY","BIRTHDATE","CREDITCHECK","CUSTOMERSEARCH","REQUESTAMOUNT","MERCHANTKEY","MERCHANTLIMIT","AVAILABLECREDIT","CHECKDATE","ORDERDATE","MERCHANTID","MERCHANTNAME","ORDERVALUE","CFIRSTORDER","CLASTORDER","CNUMBERORDERS","CORDERSAMOUNT","COPENDEBIT","CMAXWARNINGLEVEL","CLASTWARNING","EXPORTDATE"]
+		fields.each{
+			def oldValue = prop(it, old)
+			def newValue = prop(it, line)
+			def orderId = prop("ORDERID", line)
+//			if(it == 'ORDERDATE')
+//				println oldValue
+			if(oldValue!=newValue)
+				println "ERROR: $orderId -> $it doesn't match: "+oldValue +" <> "+newValue
+		}
+	}
+	
+	private static def prop(name, line){
+		try {
+			return line[name]
+		} catch (MissingPropertyException e) {
+			return null
+		}
+	}
+	
+	private static def fix(text){
+		def result
+		def previous = ""
+		def min = 100
+		text.split("\n").each{
+			def size = it.count(",")
+			if(size > 10){
+				result += previous+it+"\n"
+				previous = "" 
+			}else{
+				println it
+				previous = it
+			}
+		}
+		result.replaceAll("\"","")
 	}
 }
