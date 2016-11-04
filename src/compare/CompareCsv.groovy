7e7aecae921c81dfa097bd244a15a9c5c242b79d
Giuseppe Di Pierri <giuseppe.dipierri@gmtech.ch>
2016-11-04 20:18:54 +0100

    log improved. Splitting between info & debug

--- src/compare/CompareCsv.groovy
+++ src/compare/CompareCsv.groovy
@@ -7,62 +7,114 @@ import static com.xlson.groovycsv.CsvParser.parseCsv
 
 
 import java.nio.file.Paths
+import java.text.DecimalFormat
 
 
 class CompareCsv {
 
 	static void main(String[] args){
-		
 //		checkFiles(new File('/home/dev/pino/reports/testing/02.11.2015.csv'), new File('/home/dev/pino/reports/new/02.11.2015.csv'))
 //		checkFiles(new File('/home/dev/pino/reports/new/02.11.2015.csv'), new File('/home/dev/pino/reports/testing/02.11.2015.csv'))
 //		checkFiles(new File('/home/dev/pino/reports/testing/12.01.2016.csv'), new File('/home/dev/pino/reports/new/12.01.2016.csv'))
 //		checkFiles(new File('/home/dev/pino/reports/new/12.01.2016.csv'), new File('/home/dev/pino/reports/testing/12.01.2016.csv'))
 //		checkFiles(new File('/home/dev/pino/reports/new/03.05.2016.csv'), new File('/home/dev/pino/reports/testing/03.05.2016.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/new/03.06.2016.csv'), new File('/home/dev/pino/reports/testing/03.06.2016.csv'))
 //		checkFiles(new File('/home/dev/pino/reports/new/16.08.2016.csv'), new File('/home/dev/pino/reports/testing/16.08.2016.csv'))
+//		checkFiles(new File('/home/dev/pino/reports/new/07.06.2016.csv'), new File('/home/dev/pino/reports/testing/07.06.2016.csv'))
 		checkDirs(new File("/home/dev/pino/reports/testing"), "/home/dev/pino/reports/new")
 	}
 
+	def static report = new Report()
+	
 	private static String checkDirs(File rootFiles, String newDir) {
-		rootFiles.eachFile(){ File oldFile ->
-			if(oldFile.size() < 36551553 && oldFile.path.contains("csv")){
+		report.info "*************************"
+		report.info "Reading from Dir: "+rootFiles.path+" against dir: $newDir"
+		report.info "*************************"
+		def init = System.currentTimeMillis()
+		int count = 0
+		def files = rootFiles.listFiles().findAll{it.size() < 36551553 && it.path.endsWith(".csv")}.sort{it.name}
+		int fileNumber = files.size()
+		files.each{ File oldFile ->
+			if(oldFile.size() < 36551553 && oldFile.path.endsWith(".csv")){
 				def newFile = new File(newDir+"/"+oldFile.absolutePath.split("/").last())
 				checkFiles(newFile, oldFile)
-				checkFiles(oldFile,	newFile)
+				count++
+				def deltaNr = (System.currentTimeMillis()-init)/1000
+				def delta 	= new DecimalFormat("#####").format(deltaNr)
+				def mean 	= new DecimalFormat("#####").format(deltaNr/count)
+				report.debug "$count files read over $fileNumber in total: $delta sec (time: $mean sec/file)"
 			}
+		}
+		report.info "The End."
+	}
+	
+	static class Report{
+		def debugFile, infoFile
+		
+		Report(){
+			def dir = new File("backup")
+			if(!dir.exists()) dir.mkdirs()
+			debugFile = new File(dir, System.currentTimeMillis()+"_report_debug.txt")
+			infoFile = new File(dir, System.currentTimeMillis()+"_report_info.txt")
+		}
+		void info(message){
+			println message
+			infoFile << message +"\n"
+			debug(message)
+		}
+		void debug(message){
+			debugFile << message +"\n"
 		}
 	}
 	
 	static void checkFiles(oldFile, newFile){
-		println "************************* checking '"+oldFile.path
-		println "************************* -against '"+newFile.path+"'"
+		def init = System.currentTimeMillis()
+		def oldPath = oldFile.path
+		def newPath = newFile.path
 		def oldText = oldFile.text
 		def newText = newFile.text
+		if(oldText.trim() == ''){
+			report.debug "empty file: "+oldPath
+			return
+		}
+		if(newText.trim() == ''){
+			report.debug "empty file: "+newPath
+			return
+		}
 		def oldList = parseCsv(fix(oldText)).sort{it.ORDERID}.toList()
 		def newList = parseCsv(fix(newText)).sort{it.ORDERID}.toList()
-		
-		def oldOrderIdLine 	= oldList.groupBy{it.ORDERID}
+		def kilobytes = oldFile.length()/1000
+		report.info "************************* checking file: "+oldPath.split("/").last()+" ($kilobytes KB)"
+		check(oldList, newList, oldPath, newPath)
+//		report.debug ("**inverting**")
+		check(newList, oldList, newPath, oldPath)//inverted
+		def delta = (System.currentTimeMillis()-init)
+		report.debug "done in "+delta+" msec"
+	}
+
+	private static void check(List oldList, List newList, String oldPath, String newPath) {
+		def oldOrderIdLine 	= oldList.groupBy{primaryKey(it)}
 		def oldPersonLine 	= oldList.groupBy{secondaryKey(it)}
-		def newDateLine 	= newList.groupBy{it.CHECKDATE?.substring(0, 10)}
-		if(newDateLine.keySet().size()>1)
-			println "WARNING: file "+oldFile.path+" contains more dates: "+newDateLine.keySet()
+		def newDateLine 	= newList.groupBy{thirdKey(it)}
+		if(newDateLine.keySet().size()>1){
+			report.info "WARNING: file "+oldPath+" contains more dates: "+newDateLine.keySet()
+		}
 		for(line in newList) {
 			try {
 				if(line.MERCHANTKEY == 'mobilewalletPaycard')
 					continue;
-				def oldRecords = oldOrderIdLine[line.ORDERID]
+				def oldRecords = oldOrderIdLine[primaryKey(line)]
 				if(!oldRecords){
-					if(newDateLine[line.CHECKDATE.substring(0, 10)]?.size() != 1){
-						println "-------------------------"
-						println "ERROR: This line is available in file: "+newFile.path +" and not in "+oldFile.path
-						println "==> '$line'"
+					if(newDateLine[thirdKey(line)]?.size() != 1){
+						report.info "ERROR: This line is available in file: "+newPath +" and not in "+oldPath
+						report.info "==> '$line'"
 					}//else ignore, because in the old line there is a wrong date that we know as wrong already.
 				}else if(oldRecords.size() != 1){
 					def oldPersonRecords = oldPersonLine[secondaryKey(line)]
 					if(oldPersonRecords.size() != 1){
-						println "-------------------------"
-						println "ERROR: This line is available in file: "+newFile.path
-						println "==> '$line'"
-						println "....and $oldRecords.size() times in file '$oldFile.path': "+oldRecords
+						report.info "ERROR: This line is available in file: "+newPath
+						report.info "==> '$line'"
+						report.info "==>  and $oldRecords.size() times in file '$oldPath': "+oldRecords
 					}else{
 						match2Lines(line, oldPersonRecords.first())
 					}
@@ -73,13 +125,20 @@ class CompareCsv {
 				e.printStackTrace()
 			}
 		}
-		println "done!"
-
+//		report.debug "done!"
+	}
+	private static primaryKey(it) {
+		it.ORDERID
 	}
 
-	private static String secondaryKey(it) {
+	private static secondaryKey(it) {
 		""+it.FIRSTNAME+it.LASTNAME+it.BIRTHDATE+it.CHECKDATE
 	}
+	
+	private static thirdKey(it) {
+		it.CHECKDATE?.substring(0, 10)
+	}
+
 
 	private static match2Lines(line, old) {
 		def fields = ["CUSTOMERID","ACCOUNTID","ORDERID","GENDER","LANGUAGE","FIRSTNAME","LASTNAME","STREET","ZIP","CITY","BIRTHDATE","CREDITCHECK","CUSTOMERSEARCH","REQUESTAMOUNT","MERCHANTKEY","MERCHANTLIMIT","AVAILABLECREDIT","CHECKDATE","ORDERDATE","MERCHANTID","MERCHANTNAME","ORDERVALUE","CFIRSTORDER","CLASTORDER","CNUMBERORDERS","CORDERSAMOUNT","COPENDEBIT","CMAXWARNINGLEVEL","CLASTWARNING","EXPORTDATE"]
@@ -87,10 +146,8 @@ class CompareCsv {
 			def oldValue = prop(it, old)
 			def newValue = prop(it, line)
 			def orderId = prop("ORDERID", line)
-//			if(it == 'ORDERDATE')
-//				println oldValue
 			if(oldValue!=newValue)
-				println "ERROR: $orderId -> $it doesn't match: "+oldValue +" <> "+newValue
+				report.debug "ERROR: $orderId -> $it doesn't match: "+oldValue +" <> "+newValue
 		}
 	}
 	
@@ -105,14 +162,13 @@ class CompareCsv {
 	private static def fix(text){
 		def result
 		def previous = ""
-		def min = 100
 		text.split("\n").each{
 			def size = it.count(",")
 			if(size > 10){
 				result += previous+it+"\n"
 				previous = "" 
 			}else{
-				println it
+				report.debug it
 				previous = it
 			}
 		}
